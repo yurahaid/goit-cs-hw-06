40ad3cbcc92277073ad2d45a4d18e0c7f6c79b59
Yurii <yra-g@ukr.net>
2024-12-16 16:43:07 +0200

    📡 Implement socket writer

--- main.py
+++ main.py
@@ -2,16 +2,44 @@ import multiprocessing
 import mimetypes
 import os
 import pathlib
+from functools import partial
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import urllib.parse
 import socket
 from dotenv import load_dotenv
+import logging
 
 # Load environment variables from .env file
 load_dotenv()
 
+# Configure logging
+log_level = os.environ.get('LOG_LEVEL', 'DEBUG').upper()
+logging.basicConfig(level=getattr(logging, log_level, logging.DEBUG),
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+class SocketWriter:
+    def __init__(self, socket_ip, socket_port):
+        self.socket_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket_server = socket_ip, socket_port
+
+    def close(self):
+        self.socket_client.close()
+
+    def write(self, data):
+        self.socket_client.sendto(data, self.socket_server)
+        logging.debug(f'Send data: {data.decode()} to socket: {self.socket_server}')
+        response, address = self.socket_client.recvfrom(1024)
+        logging.debug(f'HTTP Response data: {response.decode()} from address: {address}')
+
+
 
 class HttpHandler(BaseHTTPRequestHandler):
+    def __init__(self, socket_writer: SocketWriter, *args, **kwargs):
+        self.socket_writer = socket_writer
+
+        super().__init__(*args, **kwargs)
+
+
     def do_GET(self):
         pr_url = urllib.parse.urlparse(self.path)
         if pr_url.path == '/':
@@ -21,6 +49,13 @@ class HttpHandler(BaseHTTPRequestHandler):
                 self.send_static()
             else:
                 self.send_html_file('error.html', 404)
+
+    def do_POST(self):
+        data = self.rfile.read(int(self.headers['Content-Length']))
+        self.socket_writer.write(data)
+        self.send_response(302)
+        self.send_header('Location', '/message.html')
+        self.end_headers()
 
     def send_html_file(self, filename, status=200):
         self.send_response(status)
@@ -40,45 +75,48 @@ class HttpHandler(BaseHTTPRequestHandler):
         with open(f'.{self.path}', 'rb') as file:
             self.wfile.write(file.read())
 
-
 def run_udp_server(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server = ip, port
     sock.bind(server)
     try:
-        print("Udp server started on ip: ", ip, " port: ", port)
+        logging.debug(f"UDP server started on ip: {ip}, port: {port}")
         while True:
             data, address = sock.recvfrom(1024)
-            print(f'Received data: {data.decode()} from: {address}')
+            logging.debug(f'UDP Received data: {data.decode()} from: {address}')
             sock.sendto(data, address)
-            print(f'Send data: {data.decode()} to: {address}')
+            logging.debug(f'UDP Send data: {data.decode()} to: {address}')
 
     except KeyboardInterrupt:
-        print(f'Destroy server')
+        logging.debug('Destroy UDP server')
     finally:
         sock.close()
 
 
-def run_http_server(port):
+def run_http_server(port, socket_ip, socket_port,):
+    soket_writer = SocketWriter(socket_ip, socket_port)
+
     server_address = ('', port)
-    http = HTTPServer(server_address, HttpHandler)
+    handler = partial(HttpHandler, soket_writer)
+    http = HTTPServer(server_address, handler)
 
     try:
-        print(f'Http server started on port: {port}')
+        logging.debug(f'Http server started on port: {port}')
         http.serve_forever()
     except KeyboardInterrupt:
+        logging.debug('Destroy HTTP server')
+    finally:
         http.server_close()
+        soket_writer.close()
 
 
 if __name__ == '__main__':
-    http_process = multiprocessing.Process(
-        target=run_http_server,
-        args=(int(os.environ.get('HTTP_PORT', 8000)),)
-    )
-    udp_process = multiprocessing.Process(
-        target=run_udp_server,
-        args=(os.environ.get('UDP_IP', '127.0.0.1'), int(os.environ.get('UDP_PORT', 5000)))
-    )
+    udp_ip = os.environ.get('UDP_IP', '127.0.0.1')
+    udp_port = int(os.environ.get('UDP_PORT', 5000))
+    http_port = int(os.environ.get('HTTP_PORT', 3000))
+
+    http_process = multiprocessing.Process(target=run_http_server, args=(http_port, udp_ip, udp_port,))
+    udp_process = multiprocessing.Process(  target=run_udp_server, args=(udp_ip, udp_port,))
 
     http_process.start()
     udp_process.start()
